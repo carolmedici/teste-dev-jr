84b338ab7aa22a5d6b29daed78491b26d9870ffa
carolmedici <carolina.obstetriz@gmail.com>
2023-05-06 23:44:10 -0300

    :test_tube: :bugs: teste de correção de bugs

--- src/Components/ProdutosRelacionados/index.jsx
+++ src/Components/ProdutosRelacionados/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Cards from "./Cards";
-import style from "./Produtos.module.scss";
-import produtos from "./produtos.json";
+import Cards from "../Produtos/Cards"
+import style from "./ProdutosRelacionados.module.scss"
+import produtos from "../Produtos/produtos.json";
 import { useState } from "react";
 import Slider from "react-slick";
 
@@ -9,30 +9,9 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
 
-const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
+
     
-    responsive: [
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 1,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
-    ],
-  };
+  
 
  
 
@@ -45,15 +24,11 @@ const ProdutosRelacionados = () => {
      <hr className={style.produtosStyle__linha2}/>
      
     
-     <Slider className={style.produtosStyle} {...settings} >
-     {produtos.map((produto) => (
-  <div key={produto.tag}>
-      <Cards items={items} style={style} />
-        </div>
-      ))}
+    
+      <Cards items={produtos} style={style} />
+       
 
 
-      </Slider>
      
 
 
